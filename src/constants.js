3d410f74ef664a08a9dc0929976b10b5dde08a76
feiandart <fabriziafisichella@gmail.com>
2024-07-13 18:36:47 +0200

    feat: Build Correction

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const BASE_API = "https://api.themoviedb.org/3/movie/";
+
+export const API_KEY = "efbb38f2fa751b3ef7a0254565f241fb";
+
+export const AUTH_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZmJiMzhmMmZhNzUxYjNlZjdhMDI1NDU2NWYyNDFmYiIsInN1YiI6IjY2NTYwZDM2OWIyZTQ3MzRiOTUwODBhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y7ZB2d8uP5WgPhfeGbSNeRRZXN_mT-mNlGEdTMuQ5vE";
